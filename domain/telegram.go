3ad8c9d5b287a78be8882ea1f147cee486b2cffd
Amin-MAG <M.Amin.Ghasvari@gmail.com>
2022-03-06 23:54:10 +0330

    refactor: remove domain package

--- domain/telegram.go
+++
@@ -1,8 +0,0 @@
-package domain
-
-import "legato_server/api"
-
-type TelegramUseCase interface {
-	AddToScenario(u *api.UserInfo, scenarioId uint, nh api.NewServiceNodeRequest) (api.ServiceNodeResponse, error)
-	Update(u *api.UserInfo, scenarioId uint, nodeId uint, nt api.NewServiceNodeRequest) error
-}
