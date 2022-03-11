d5d0fa5e20b7884475484d73613fced37356add2
Amin-MAG <M.Amin.Ghasvari@gmail.com>
2022-03-11 15:10:58 +0330

    refactor: remove unnecessary scheduling codes

--- router/scenario.go
+++ router/scenario.go
@@ -3,8 +3,6 @@ package router
 import (
 	"fmt"
 	"legato_server/api"
-	"legato_server/scheduler"
-	"log"
 	"net/http"
 	"strconv"
 
@@ -15,94 +13,12 @@ var scenarioRG = routeGroup{
 	name: "User Scenario",
 	routes: routes{
 		route{
-			name:        "Schedule a scenario",
-			method:      POST,
-			pattern:     "/users/:username/scenarios/:scenario_id/schedule",
-			handlerFunc: scheduleScenario,
-		},
-		route{
-			name:        "Force a scenario to start",
-			method:      POST,
-			pattern:     "/scenarios/:scenario_id/force",
-			handlerFunc: forceStartScenario,
-		},
-		route{
 			name:        "Set an interval for a scenario",
 			method:      PUT,
 			pattern:     "/users/:username/scenarios/:scenario_id/set-interval",
 			handlerFunc: setScenarioInterval,
 		},
 	},
-}
-
-func forceStartScenario(c *gin.Context) {
-	scenarioId, _ := strconv.Atoi(c.Param("scenario_id"))
-
-	sss := api.NewStartScenarioSchedule{}
-	_ = c.BindJSON(&sss)
-
-	// Check if it is scheduler
-	// It should be more secure later
-	if c.GetHeader("Authorization") != scheduler.AccessToken {
-		log.Println(c.GetHeader("Authorization"))
-		log.Println(scheduler.AccessToken)
-		c.JSON(http.StatusForbidden, gin.H{
-			"message": "You do not have the access to force",
-		})
-		return
-	}
-
-	// Start that scenario because of the scheduler signal
-	err := resolvers.ScenarioUseCase.ForceStartScenario(uint(scenarioId), sss.Token)
-	if err != nil {
-		log.Println(err)
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"message": fmt.Sprintf("can not force start this scenario: %s", err),
-		})
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{
-		"message": "scenario is started successfully",
-	})
-}
-
-func scheduleScenario(c *gin.Context) {
-	username := c.Param("username")
-	scenarioId, _ := strconv.Atoi(c.Param("scenario_id"))
-
-	sss := api.NewStartScenarioSchedule{}
-	_ = c.BindJSON(&sss)
-
-	// Auth
-	loginUser := checkAuth(c, []string{username})
-	if loginUser == nil {
-		return
-	}
-
-	log.Printf("Request new schedule for scenairo %d in %+v", scenarioId, sss.ScheduledTime)
-
-	// Schedule that scenario
-	err := resolvers.ScenarioUseCase.Schedule(loginUser, uint(scenarioId), &sss)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"message": fmt.Sprintf("can not schedule this scenario: %s", err),
-		})
-		return
-	}
-
-	scenario, err := resolvers.ScenarioUseCase.GetUserScenarioById(loginUser, uint(scenarioId))
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"message": fmt.Sprintf("can not fetch this scenario: %s", err),
-		})
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{
-		"message":  fmt.Sprintf("scenario is scheduled successfully for %v", sss.ScheduledTime),
-		"scenario": scenario,
-	})
 }
 
 func setScenarioInterval(c *gin.Context) {
--- scenario/usecase/scenario_usercase.go
+++ scenario/usecase/scenario_usercase.go
@@ -6,10 +6,8 @@ import (
 	"errors"
 	"fmt"
 	"legato_server/api"
-	"legato_server/domain"
-	"legato_server/env"
-	"legato_server/helper/converter"
 	"legato_server/internal/legato/database/postgres"
+	"legato_server/internal/scheduler/api/rest/schedule"
 	"log"
 	"net/http"
 	"time"
@@ -25,76 +23,6 @@ func NewScenarioUseCase(db *postgres.LegatoDB, timeout time.Duration) domain.Sce
 		db:             db,
 		contextTimeout: timeout,
 	}
-}
-
-// ForceStartScenario is not accessible for users.
-// It is used for starting the scheduled scenarios by scheduler.
-func (s scenarioUseCase) ForceStartScenario(scenarioId uint, scheduleToken []byte) error {
-	scenario, err := s.db.GetScenarioById(scenarioId)
-	if err != nil {
-		return err
-	}
-
-	log.Println(scenario.ScheduleToken)
-	log.Println(scheduleToken)
-	if !bytes.Equal(scenario.ScheduleToken, scheduleToken) {
-		return errors.New("unfortunately the token has been expired")
-	}
-
-	if scenario.IsActive == nil {
-		return errors.New("this scenario has null isActive field")
-	}
-
-	if !(*scenario.IsActive) {
-		return errors.New("this scenario is inactive")
-	}
-
-	err = scenario.Start()
-	if err != nil {
-		return err
-	}
-
-	err = scenario.Schedule(scheduleToken)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (s scenarioUseCase) Schedule(u *api.UserInfo, scenarioId uint, schedule *api.NewStartScenarioSchedule) error {
-	user := converter.UserInfoToUserDb(*u)
-	_, err := s.db.GetUserScenarioById(&user, scenarioId)
-	if err != nil {
-		return err
-	}
-
-	// Update the time and interval for this scenario
-	err = s.db.UpdateScenarioScheduleInfoById(&user, scenarioId, schedule.ScheduledTime, schedule.Interval)
-	if err != nil {
-		return err
-	}
-
-	// Generate a new schedule token
-	token, err := s.db.SetNewScheduleToken(&user, scenarioId)
-	if err != nil {
-		return err
-	}
-	schedule.Token = token
-
-	// Make http request to enqueue this job
-	schedulerUrl := fmt.Sprintf("%s/api/schedule/scenario/%d", env.ENV.SchedulerUrl, scenarioId)
-	body, err := json.Marshal(schedule)
-	if err != nil {
-		return err
-	}
-	reqBody := bytes.NewBuffer(body)
-	_, err = http.Post(schedulerUrl, "application/json", reqBody)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
 
 func (s scenarioUseCase) SetInterval(u *api.UserInfo, scenarioId uint, interval *api.NewScenarioInterval) error {
@@ -123,7 +51,7 @@ func (s scenarioUseCase) SetInterval(u *api.UserInfo, scenarioId uint, interval 
 	}
 	if *scenario.IsActive {
 		minutes := time.Duration(scenario.Interval) * time.Minute
-		schedule := &api.NewStartScenarioSchedule{
+		schedule := &schedule.NewStartScenarioSchedule{
 			ScheduledTime: time.Now().Add(minutes),
 			SystemTime:    time.Now(),
 		}
